b42deb317945f3ea714cb3c82c9d703f08b5dcf8
bijaypokhrel05 <077bei011@ioepc.edu.np>
2025-08-04 20:36:01 +0545

    feat: solved from  2.0 - 2.5 exercises

---
+++ part2/courseinfo/src/App.jsx
@@ -0,0 +1,90 @@
+const App = () => {
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+
+  function Course({ course }) {
+    function Header({ name }) {
+      return <h2>{name}</h2>
+    }
+
+    function Content({ parts }) {
+      return parts.map(part => {
+        return <Part key={part.id} name={part.name} exercises={part.exercises} />
+      })
+    }
+
+    function Part({ name, exercises }) {
+      return <p>{name} {exercises}</p>
+    }
+
+    function Total({ parts }) {
+      return (<h4>total of {parts.reduce((res, curr) => {
+        res += curr.exercises;
+        return res;
+      }, 0)} exercises</h4>
+      )
+    }
+
+    return (
+      <>
+        <Header name={course.name} />
+        <Content parts={course.parts} />
+        <Total parts={course.parts} />
+      </>
+    )
+  }
+
+  return (
+    <>
+      <h1>Web development curriculum</h1>
+      {courses.map(course => {
+        return <Course key={course.id} course={course} />
+      })
+      }
+    </>
+  )
+};
+
+export default App;
