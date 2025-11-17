71c8619865c575231c9a148ca394d536c692d588
bijaypokhrel05 <077bei011@ioepc.edu.np>
2025-11-17 23:36:55 +0545

    refactor: separate them in different component and logic is same, completed exercise 5.6

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -1,65 +1,94 @@
 import { useState, useEffect } from 'react'
-import Blog from './components/Blog';
-import blogService from './services/blogs';
-import Login from './components/Login';
-import CreateNew from './components/CreateNew';
-import Togglable from './components/Togglable';
+import Blog from './components/Blog'
+import blogService from './services/blogs'
+import Login from './components/Login'
+import CreateNew from './components/CreateNew'
+import Togglable from './components/Togglable'
+
+// Separate component for displaying list of blogs
+const Blogs = ({ blogs }) => (
+  <div>
+    {blogs.map(blog => (
+      <Blog key={blog.id} blog={blog} />
+    ))}
+  </div>
+)
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
-  const [name, setName] = useState('');
-  const [isLoggedOut, setIsLoggedOut] = useState(true);
-  const [notification, setNotification] = useState(null);
-  const [blogsVisible, setBlogsVisible] = useState(true);
+  const [name, setName] = useState('')
+  const [isLoggedOut, setIsLoggedOut] = useState(true)
+  const [notification, setNotification] = useState(null)
+  const [blogsVisible, setBlogsVisible] = useState(true)
 
-
+  // Fetch blogs once on initial render
   useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs(blogs)
-    )  
-  }, [blogs])
+    blogService.getAll().then(allBlogs => {
+      setBlogs(allBlogs)
+    })
+  }, [])
 
- useEffect(() => {
-    if (notification) {
-      const timer = setTimeout(() => {
-        setNotification(null)
-      }, 5000)
+  // Notification timeout
+  useEffect(() => {
+    if (!notification) return
 
-      return () => clearTimeout(timer) // cleanup
-    }
+    const timer = setTimeout(() => {
+      setNotification(null)
+    }, 5000)
+
+    return () => clearTimeout(timer)
   }, [notification])
-  
-  const Blogs = () => {
-    return (
-      <div>
-        {blogs.map(blog =>
-            <Blog key={blog.id} blog={blog} />
-          )}
-      </div>
-    )
+
+  const handleLogout = () => {
+    setIsLoggedOut(true)
+    localStorage.removeItem('token')
   }
 
   return (
     <div>
-      {isLoggedOut ? <Login setIsLoggedOut={setIsLoggedOut} setName={setName} setNotification={setNotification}/>
-       : (
+      {isLoggedOut ? (
+        <Login 
+          setIsLoggedOut={setIsLoggedOut} 
+          setName={setName} 
+          setNotification={setNotification} 
+        />
+      ) : (
         <div>
           <h2>blogs</h2>
-          {notification && 
-          <p style={{backgroundColor: 'silver', color: 'green', fontSize: '20px', width: 'auto', height: '45px', borderRadius: '5px', border: '3px solid green', paddingLeft: '10px', display: 'flex', alignItems: 'center'}}
-            >{notification}</p> }
+
+          {notification && (
+            <p
+              style={{
+                backgroundColor: 'silver',
+                color: 'green',
+                fontSize: '20px',
+                width: 'auto',
+                height: '45px',
+                borderRadius: '5px',
+                border: '3px solid green',
+                paddingLeft: '10px',
+                display: 'flex',
+                alignItems: 'center'
+              }}
+            >
+              {notification}
+            </p>
+          )}
+
           <p>
-            {name} is logged in 
-            <button onClick={() => {
-              setIsLoggedOut(true);
-              localStorage.removeItem('token');
-              }}>logout</button>
+            {name} is logged in{' '}
+            <button onClick={handleLogout}>logout</button>
           </p>
 
-          <Togglable buttonLabel='create new blog' blogsVisible={blogsVisible} setBlogsVisible={setBlogsVisible}>
+          <Togglable
+            buttonLabel="create new blog"
+            blogsVisible={blogsVisible}
+            setBlogsVisible={setBlogsVisible}
+          >
             <CreateNew setNotification={setNotification} />
           </Togglable>
-          {blogsVisible && <Blogs />}
+
+          {blogsVisible && <Blogs blogs={blogs} />}
         </div>
       )}
     </div>
