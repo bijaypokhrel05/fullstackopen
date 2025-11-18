20ee704137c36fabad9720e4fc447edff03a8470
bijaypokhrel05 <077bei011@ioepc.edu.np>
2025-11-19 00:01:26 +0545

    feat: completed exercise 5.7

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,24 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const [isDetails, setIsDetails] = useState(false);
+
+  const handleDetails = () => {
+    setIsDetails(prev => !prev);
+  }
+  return (
+    <div style={{height: 'auto', width: '100%', border: '1px solid black', marginBottom: '5px', paddingLeft: '2px', paddingTop: '10px'}}>
+    {blog.title} {blog.author} {' '}
+    <button onClick={handleDetails}>{isDetails ? 'hide' : 'view'}</button>
+    {isDetails && (
+      <>
+        <div>{blog.url}</div>
+        <div>likes {blog.likes} <button>likes</button></div>
+        <div>{blog.user.name}</div>
+      </>
+  )}
   </div>  
-)
+  )
+}
 
 export default Blog
